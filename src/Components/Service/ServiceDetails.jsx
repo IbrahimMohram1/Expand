b3c7c3a5cb6659bbe820fafc0772decb9d4a86b0
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-03-09 17:24:41 +0200

    Update

--- src/Components/Service/ServiceDetails.jsx
+++ src/Components/Service/ServiceDetails.jsx
@@ -22,12 +22,12 @@ export default function Service() {
       <h2 className="mainColor text-2xl my-3 text-center">Our Services</h2>
 
       <div className="w-[85%] mx-auto">
-        <div className="flex justify-center md:flex-row flex-col ">
+        <div className="grid grid-cols-2 gap-2">
           {Services.length > 0 ? (
             Services.map((service) => (
               <div
                 key={service.id}
-                className="w-1/2 mx-2 my-2 text-nowrap md:w-1/2 flex justify-between items-center"
+                className="text-nowrap flex justify-between items-center"
               >
                 <Link to={`/services/${service.id}`}>
                   <p className="text-xs">{service.title}</p>
@@ -35,7 +35,7 @@ export default function Service() {
               </div>
             ))
           ) : (
-            <p>No Services Found </p>
+            <p>No Services Found</p>
           )}
         </div>
       </div>
