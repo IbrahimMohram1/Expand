53ea771ea8c106a355ff7eef4fd62fd5a19213a9
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-03-09 18:05:09 +0200

    Update

--- src/Components/HeroSection/HeroSlider.jsx
+++ src/Components/HeroSection/HeroSlider.jsx
@@ -35,7 +35,7 @@ export default function HeroSection() {
       </div>
 
       {/* الـ HR والعناصر التحتية */}
-      <div className="absolute z-10 w-[90%] md:w-[80%] mx-auto pb-8 bottom-14">
+      <div className="absolute w-full z-10  md:w-[80%] mx-auto pb-8 md:bottom-14 bottom-6 justify-center flex">
         <hr className="h-[.75px] bg-slate-200" />
         <div className="flex  md:flex-row justify-between items-center gap-4 mt-4">
           <div className="flex items-center gap-x-2 text-white">
