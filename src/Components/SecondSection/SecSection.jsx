c1ae724064b86523374ebab063e88b68318d3e89
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-03-10 22:29:27 +0200

    Update

--- src/Components/SecondSection/SecSection.jsx
+++ src/Components/SecondSection/SecSection.jsx
@@ -7,13 +7,16 @@ export default function SecSection({ image, who, p }) {
       <div className="container my-5">
         <div className="m-auto">
           <div className="flex justify-center items-start md:flex-row flex-col gap-x-2 md:gap-y-0 gap-y-3">
-            <div className="md:w-1/2 my-1 w-full">
+            {/* الصورة */}
+            <div className="md:w-1/2 my-1 w-full flex items-start">
               <img
                 src={image}
-                className="w-full h-full object-cover rounded-xl"
+                className="w-full h-auto max-h-full object-contain rounded-xl"
+                style={{ height: "auto" }}
               />
             </div>
 
+            {/* النص */}
             <div className="md:w-1/2 w-full">
               <h1 className="mainColor text-2xl tracking-[1px] my-3">{who}</h1>
               <p className="text-xs leading-5 tracking-[1.5px]">{p}</p>
