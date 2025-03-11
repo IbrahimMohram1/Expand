94714e018c21dbce96b6ae9333badfd6d77a92ca
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-03-11 21:20:58 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
+import "./Font.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Layout from "./Components/Layout/Layout";
