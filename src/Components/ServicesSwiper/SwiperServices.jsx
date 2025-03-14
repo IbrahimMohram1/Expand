9c3501ea4256c4d18aea643ef1fd2e1a99b1a7ac
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-03-14 22:42:41 +0200

    'update'

--- src/Components/ServicesSwiper/SwiperServices.jsx
+++ src/Components/ServicesSwiper/SwiperServices.jsx
@@ -36,7 +36,7 @@ export default function SwiperServices() {
     slidesToShow: 5,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: "100px",
+    centerPadding: "0px",
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
       { breakpoint: 640, settings: { slidesToShow: 2, slidesToScroll: 1 } },
